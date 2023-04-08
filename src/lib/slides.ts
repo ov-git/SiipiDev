57056ceee7c3aa9fff614f9dd153d0836400a680
ov-git <virtaoskari@gmail.com>
2023-04-08 20:40:31 +0300

    deploy

--- src/lib/slides.ts
+++ src/lib/slides.ts
@@ -1,5 +1,5 @@
-import aula from "../assets/aula2.jpg";
-import Taulut2 from "../assets/Taulut2.jpg";
-import TerassiSiipi from "../assets/TerassiSiipi.jpg";
+import aula from "../assets/slider-images/Aula.jpg";
+import Taulut from "../assets/slider-images/Taulut.jpg";
+import TerassiSiipi from "../assets/slider-images/TerassiSiipi.jpg";
 
-export const slides = [aula, TerassiSiipi, Taulut2];
+export const slides = [aula, TerassiSiipi, Taulut];
